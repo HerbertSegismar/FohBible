6d08e680e1312534dfa196d470090ab3138b9ded
HerbertSegismar <herbert.segismar@gmail.com>
2026-03-12 22:07:52 +1300

    Package name change. com.example.... to com.fountofhopedotorg....

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fohbible"
+    namespace = "com.fountofhopedotorg.fohbible"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.fohbible"
+        applicationId = "com.fountofhopedotorg.fohbible"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
