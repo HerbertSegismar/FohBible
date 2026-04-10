3241c81dc128bc6057ec3d446638c1aef718fa62
HerbertSegismar <herbert.segismar@gmail.com>
2026-04-10 21:59:35 +1200

    removed scrollstate, now app uses lazylist only for best performance on all devices even for old ones*

--- app/src/main/java/com/fountofhopedotorg/fohbible/screens/SettingsScreen.kt
+++ app/src/main/java/com/fountofhopedotorg/fohbible/screens/SettingsScreen.kt
@@ -1299,7 +1299,7 @@ fun AboutDialog(onDismiss: () -> Unit) {
         text = {
             Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
                 Text(
-                    "Version: 1.0.3",
+                    "Version: 1.0.4",
                     style = MaterialTheme.typography.bodyLarge,
                     fontWeight = FontWeight.Medium
                 )
